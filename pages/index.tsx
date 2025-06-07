bc153aa8cd58a9f6623ed541ca6e764b9fc5afff
Daniel Kiamba <treydancan@gmail.com>
2025-06-07 16:06:17 +0300

    Update index.tsx
    Updated description

--- pages/index.tsx
+++ pages/index.tsx
@@ -63,7 +63,7 @@ export default function Genoo() {
           <div className="absolute inset-0 bg-black bg-opacity-50"></div>
           <div className="relative z-10">
             <h1 className="text-4xl font-bold mb-4">Welcome to GENO Labs</h1>
-            <p className="text-xl">Offering Biochemistry, Hematology, and Cytology Diagnostic Services</p>
+            <p className="text-xl">Offering Biochemistry, Hematology, Histology and Cytology Diagnostic Services</p>
           </div>
         </header>
 
